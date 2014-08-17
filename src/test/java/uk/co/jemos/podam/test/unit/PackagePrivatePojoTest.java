3003c2b61379fc6a6657bb302203a423a8b4b1d0
Bradley Baetz <bbaetz@gmail.com>
2014-08-17 19:48:25 +1000

    Test that the appropriate warning messages for non-public accessors are being reported only when appropriate

--- src/test/java/uk/co/jemos/podam/test/unit/PackagePrivatePojoTest.java
+++ src/test/java/uk/co/jemos/podam/test/unit/PackagePrivatePojoTest.java
@@ -2,7 +2,18 @@ package uk.co.jemos.podam.test.unit;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verify;
+import org.powermock.api.mockito.mockpolicies.Slf4jMockPolicy;
+import org.powermock.core.classloader.annotations.MockPolicy;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.LoggerFactory;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -10,13 +21,15 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  * @author daivanov
  *
  */
+@RunWith(PowerMockRunner.class)
+@MockPolicy(Slf4jMockPolicy.class)
 public class PackagePrivatePojoTest {
-
 	@Test
-	public void testPackagePrivateClassInstantiation(){
+	public void testPackagePrivateClassInstantiation() {
 		PodamFactory factory = new PodamFactoryImpl();
 		PackagePrivatePojo pojo = factory.manufacturePojo(PackagePrivatePojo.class);
 		Assert.assertNotNull(pojo);
 		Assert.assertNotNull(pojo.getValue());
+		verify(LoggerFactory.getLogger(PodamFactoryImpl.class.getName()), never()).warn(anyString(), eq("setValue"));
 	}
 }
--- src/test/java/uk/co/jemos/podam/test/unit/PrivateSetterPojoTest.java
+++ src/test/java/uk/co/jemos/podam/test/unit/PrivateSetterPojoTest.java
@@ -2,15 +2,26 @@ package uk.co.jemos.podam.test.unit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import org.powermock.api.mockito.mockpolicies.Slf4jMockPolicy;
+import org.powermock.core.classloader.annotations.MockPolicy;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.LoggerFactory;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
+@RunWith(PowerMockRunner.class)
+@MockPolicy(Slf4jMockPolicy.class)
 public class PrivateSetterPojoTest {
 	@Test
-	public void testPrivateAccessors() {
+	public void testPrivateAccessors(){
 		PodamFactory factory = new PodamFactoryImpl();
 		PrivateSetterPojo pojo = factory.manufacturePojo(PrivateSetterPojo.class);
 		Assert.assertNotNull(pojo);
 		Assert.assertNotNull(pojo.getValue());
+		verify(LoggerFactory.getLogger(PodamFactoryImpl.class.getName())).warn(anyString(), eq("setValue"));
 	}
 }
